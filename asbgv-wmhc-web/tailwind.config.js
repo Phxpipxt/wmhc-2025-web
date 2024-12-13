cf529c65f42042216cacab9a479432408fec5ded
Phxpipxt <112983043+Phxpipxt@users.noreply.github.com>
2024-12-13 14:38:17 +0700

    Re-upload to Git

---
+++ asbgv-wmhc-web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    './node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
